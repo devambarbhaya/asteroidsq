b5f6d4500f925b394f8c275da5eb4eb2f35c25db
devambarbhaya <barbhaya.devam@gmail.com>
2024-12-26 10:12:05 +0000

    add FPS

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import pygame
 from constants import *
 
 def main():
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(f"Screen height: {SCREEN_HEIGHT}")
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  dt = 0
+  clock = pygame.time.Clock()
   
   while True:
     for event in pygame.event.get():
@@ -14,6 +13,7 @@ def main():
         return
       
     screen.fill((0, 0, 0))
+    dt = clock.tick(60) / 1000
     pygame.display.flip()
   
 if __name__ == "__main__":
